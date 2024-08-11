0bb9f4294f526f5d8b0adbeb00a18e3b37a6763c
Ryan <ryansouzadev@gmail.com>
2024-08-11 13:38:58 -0300

    feat: create project and authContext

---
+++ src/context/AuthProvider/util.ts
@@ -0,0 +1,31 @@
+import Api from "@/services/api"
+import { IUser } from "./types";
+
+export function setUserLocalStorage(user: IUser | null) {
+    localStorage.setItem('u', JSON.stringify(user))
+}
+
+export function getUserLocalStorage() {
+    const json = localStorage.getItem('u');
+
+    if (!json) {
+        throw new Error('No user data found in localStorage');
+    }
+
+    try {
+        const user = JSON.parse(json);
+        return user;
+    } catch {
+        throw new Error('Failed to parse user data from localStorage');
+    }
+}
+
+export async function LoginRequest (email: string, password: string) {
+    try {
+        const request = await Api.post('login', { email, password})
+
+        return request.data;
+    } catch {
+        throw new Error('Failed to log in. Please check your credentials and try again.');
+    }
+}
